01f1683833b3934c3faf0957aec568d9cfccbafb
Serhii <sergiomakafon@gmail.com>
2022-05-26 21:19:58 +0300

    Modal windows added

--- src/js/script.js
+++ src/js/script.js
@@ -46,5 +46,21 @@ $(document).ready(function(){
     
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+    // Modal
+    $('[data-modal=consultation]').on('click',function() {
+      $('.overlay, #consultation').fadeIn('1000');
+    });
+
+    $('.modal__close').on('click',function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('1000');
+    });
+
+    $('.button--mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('1000');
+      })
+    });
+
 
  });
